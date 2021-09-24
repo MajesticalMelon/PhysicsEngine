95dd3c467970eab60e850f28e81dc38870012f40
MajesticalMelon <owenfriend2002@gmail.com>
2021-09-24 11:13:59 -0400

    Delete useless import

--- Display/Canvas.java
+++ Display/Canvas.java
@@ -6,7 +6,6 @@ import java.awt.Color;
 import java.awt.Image;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.geom.AffineTransform;
 import java.io.File;
 import java.io.IOException;
 
